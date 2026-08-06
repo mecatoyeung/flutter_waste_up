a8b1bf1a036644556ed67b1248a6d20c0bd2b91f
Cato Yeung <me@catoyeung.com>
2026-08-06 15:43:03 +0800

    Fix Github actions error

--- frontend/test/widget_test.dart
+++ frontend/test/widget_test.dart
@@ -5,9 +5,5 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:flutter_test/flutter_test.dart';
-
-import 'package:waste_up/main.dart';
-
 void main() {
 }
